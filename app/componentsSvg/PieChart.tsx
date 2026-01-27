c7c1083bc4933f799fee3984d71aff3cd8041e2d
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-01-27 11:43:56 +0200

    feat: create PieChart component

---
+++ app/componentsSvg/PieChart.tsx
@@ -0,0 +1,9 @@
+import { View } from "react-native";
+
+type PieChartProps = {
+  data: { x: number; y: number }[];
+};
+
+export default function PieChart({ data }: PieChartProps) {
+  return <View></View>;
+}
