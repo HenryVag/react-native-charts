17f63aa3511d85b0c6d6d4dc1e444322cd27d13c
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-01-28 14:29:19 +0200

    refactor: created helper for PieChart UI calculations

--- app/componentsSvg/PieChart.tsx
+++ app/componentsSvg/PieChart.tsx
@@ -1,50 +1,55 @@
 import { View } from "react-native";
 import Svg from "react-native-svg";
-import Sector from "../componentsSvg/Sector";
+import Sector, { SectorLine } from "../componentsSvg/Sector";
+import computePieChart from "./utils/computePieChart";
 
 const colors = ["#95D0E8", "#A179AA", "#C4A3CD", "#BDE2F3", "#E8F3F9"];
 
 type PieChartProps = {
   data: { x: number; y: number }[];
+  radius: number;
 };
 
-export default function PieChart({ data }: PieChartProps) {
-  let startAngle = 90;
+export default function PieChart({ data, radius }: PieChartProps) {
   let i = 0;
-  const answeredTotal = data.reduce(function (acc, curr) {
-    return (acc += curr.y);
-  }, 0);
 
-  function calculateEndAngle(
-    startAngle: number,
-    answeredQst: number,
-    answeredTotal: number,
-  ) {
-    let endAngle = (answeredQst / answeredTotal) * 360 + startAngle;
-    return endAngle;
-  }
-
+  let { sectorData, lineData } = computePieChart(data, radius);
+  console.log(sectorData[0]);
   return (
     <View>
       <Svg width="450" height="450" viewBox="0 0 100 100">
-        {data.map((obj) => {
-          const endAngle = calculateEndAngle(startAngle, obj.y, answeredTotal);
+        {sectorData.map((obj) => {
           let fillColor = colors[i];
           const sector = (
             <Sector
               startX={50}
               startY={50}
-              startAngle={startAngle}
-              endAngle={endAngle}
-              radius={40}
+              startAngle={obj.startAngle}
+              endAngle={obj.endAngle}
+              radius={obj.radius}
               fill={fillColor}
-              key={obj.x}
+              key={obj.key}
             />
           );
 
-          startAngle = endAngle;
           i++;
           return sector;
+        })}
+
+        {lineData.map((obj) => {
+          console.log(obj.key, obj.lineY);
+          const sectorLine = (
+            <SectorLine
+              startX={50}
+              startY={50}
+              lineX={obj.lineX}
+              lineY={obj.lineY}
+              radius={obj.radius}
+              key={obj.key}
+            />
+          );
+
+          return sectorLine;
         })}
       </Svg>
     </View>
---
+++ app/componentsSvg/utils/computePieChart.ts
@@ -0,0 +1,56 @@
+// Calculates all necessary values for the PieChart component
+
+import { calcLineX, calcLineY } from "./computeSector";
+
+type SectorDataProps = {
+  radius: number;
+  startAngle: number;
+  endAngle: number;
+  key: number;
+};
+
+type LineDataProps = {
+  radius: number;
+  lineX: number;
+  lineY: number;
+  key: number;
+};
+
+export default function computePieChart(
+  data: { x: number; y: number }[],
+  radius: number,
+) {
+  let sectorData: SectorDataProps[] = [];
+  let lineData: LineDataProps[] = [];
+
+  let startAngle = 90;
+
+  const answeredTotal = data.reduce(function (acc, curr) {
+    return (acc += curr.y);
+  }, 0);
+
+  data.forEach((obj) => {
+    const endAngle = calculateEndAngle(startAngle, obj.y, answeredTotal);
+    let lineX = calcLineX(radius, endAngle);
+    let lineY = calcLineY(radius, endAngle);
+    let key = obj.x;
+
+    let sector = { radius, startAngle, endAngle, key };
+    let line = { radius, lineX, lineY, key };
+
+    startAngle = endAngle;
+    sectorData.push(sector);
+    lineData.push(line);
+  });
+
+  return { sectorData, lineData };
+}
+
+function calculateEndAngle(
+  startAngle: number,
+  answeredQst: number,
+  answeredTotal: number,
+) {
+  let endAngle = (answeredQst / answeredTotal) * 360 + startAngle;
+  return endAngle;
+}
