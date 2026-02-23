876e5b20393edc6d33bd87342e30c8d7d2e69401
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-02-23 13:48:45 +0200

    chore: fix imports

--- app/componentsSvg/native/PieChart/Sector.tsx
+++ app/componentsSvg/native/PieChart/Sector.tsx
@@ -1,5 +1,5 @@
 import { Path } from "react-native-svg";
-import computeSector from "../PieChart/utils/PieChart/computeSector";
+import computeSector from "../../utils/PieChart/computeSector";
 
 type SectorProps = {
   /** Drawing startpoint x */
