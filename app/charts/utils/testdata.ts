4e7e66845374bac52fb3aeffa263190f7cb664ed
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-03-05 17:56:00 +0200

    chore: add comments

--- app/charts/utils/testdata.ts
+++ app/charts/utils/testdata.ts
@@ -1,8 +1,10 @@
+//Modify test data here
+
 const testData = [
     { group: "1", value: 1, fill: "#95d0e8" },
-    { group: "2", value: 1, fill: "#A179AA" },
+    { group: "2", value: 0, fill: "#A179AA" },
     { group: "3", value: 0, fill: "#C4A3CD" },
-    { group: "4", value: 1, fill: "#BDE2F3" },
+    { group: "4", value: 0, fill: "#BDE2F3" },
     { group: "5", value: 0, fill: "#E8F3F9" },
     { group: "6", value: 0, fill: "#E8F3F9" },
 ];
