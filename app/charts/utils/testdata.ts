3eecd5d82bf12b706c29d079cf950987c85a6f99
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-03-09 19:34:11 +0200

    chore: modify testData groups

--- app/charts/utils/testdata.ts
+++ app/charts/utils/testdata.ts
@@ -1,12 +1,12 @@
 //Modify test data here
 
 const testData = [
-    { group: "1", value: 1, fill: "#95d0e8" },
-    { group: "2", value: 0, fill: "#A179AA" },
-    { group: "3", value: 0, fill: "#C4A3CD" },
-    { group: "4", value: 0, fill: "#BDE2F3" },
-    { group: "5", value: 0, fill: "#E8F3F9" },
-    { group: "6", value: 0, fill: "#E8F3F9" },
+    { group: "g1", value: 1, fill: "#95d0e8" },
+    { group: "g2", value: 1, fill: "#A179AA" },
+    { group: "g3", value: 0, fill: "#C4A3CD" },
+    { group: "g4", value: 0, fill: "#BDE2F3" },
+    { group: "g5", value: 0, fill: "#E8F3F9" },
+    { group: "g6", value: 0, fill: "#E8F3F9" },
 ];
 
 export default testData
