9cc13d2dc7724e7d969a2df23381284a50f1b6ab
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-03-12 14:56:04 +0200

    chore: restructure folders

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
-import { View } from "react-native";
-import PieChart from "./charts/PieChart/PieChart";
-import testData from "./charts/utils/testdata";
+import PieChart from "@/charts/PieChart/PieChart";
+import testData from "@/charts/utils/test-data";
+import { Text, View } from "react-native";
 
 export default function Index() {
   return (
@@ -14,15 +14,26 @@ export default function Index() {
     >
       <PieChart
         data={testData}
-        radius={50}
+        radius={10}
         showLabels={true}
         labelFont="Poppins_400Regular"
         strokeWidth={0.5}
         labelFontSize={4}
-        labelDistance={1.4}
-        
+        labelDistance={1.4} 
         sectorStrokeWidth={1}
         sectorStrokeWidthThreshold={80}
+        legend={(sectorData) => (
+            <View>
+              {sectorData.map((sector) => (
+                <View key={sector.group} style={{ flexDirection: "row", alignItems: "center" }}>
+                  <View style={{ width: 12, height: 12, backgroundColor: sector.fill }} />
+                  <Text>{sector.group}: {sector.label}</Text>
+                </View>
+              ))}
+            </View>
+          )}
+        legendPosition="left"
+        
       />
     </View>
   );
