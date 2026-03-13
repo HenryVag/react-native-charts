ff052dfc855395dd8d33dc1217b596c6a37f6fcc
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-03-13 15:58:47 +0200

    feat: add default legend prop

--- app/index.tsx
+++ app/index.tsx
@@ -1,40 +1,31 @@
-import PieChart from "@/charts/PieChart/PieChart";
-import testData from "@/charts/utils/test-data";
-import { Text, View } from "react-native";
+import { View } from "react-native"
+import PieChart from "@/charts/piechart/piechart"
+import { ExampleLegend } from "@/charts/utils/default-props"
+import testData from "@/charts/utils/test-data"
 
 export default function Index() {
-  return (
-    <View
-      style={{
-        marginTop: 0,
-        alignItems: "center",
-        justifyContent: "center",
-        height: 600,
-      }}
-    >
-      <PieChart
-        data={testData}
-        radius={10}
-        showLabels={true}
-        labelFont="Poppins_400Regular"
-        strokeWidth={0.5}
-        labelFontSize={4}
-        labelDistance={1.4} 
-        sectorStrokeWidth={1}
-        sectorStrokeWidthThreshold={80}
-        legend={(sectorData) => (
-            <View>
-              {sectorData.map((sector) => (
-                <View key={sector.group} style={{ flexDirection: "row", alignItems: "center" }}>
-                  <View style={{ width: 12, height: 12, backgroundColor: sector.fill }} />
-                  <Text>{sector.group}: {sector.label}</Text>
-                </View>
-              ))}
-            </View>
-          )}
-        legendPosition="left"
-        
-      />
-    </View>
-  );
+	return (
+		<View
+			style={{
+				marginTop: 0,
+				alignItems: "center",
+				justifyContent: "center",
+				height: 600,
+			}}
+		>
+			<PieChart
+				data={testData}
+				radius={10}
+				showLabels={true}
+				labelFont="Poppins_400Regular"
+				strokeWidth={0.5}
+				labelFontSize={4}
+				labelDistance={1.4}
+				sectorStrokeWidth={1}
+				sectorStrokeWidthThreshold={80}
+				legend={(data) => <ExampleLegend data={data} />}
+				legendPosition="left"
+			/>
+		</View>
+	)
 }
