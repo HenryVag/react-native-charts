6c1330f0922dd52c2c5c123f3088f992c6d6b63e
HenryVag <146335816+HenryVag@users.noreply.github.com>
2026-04-10 21:20:04 +0300

    chore: add test data

--- charts/utils/test-data.ts
+++ charts/utils/test-data.ts
@@ -1,6 +1,6 @@
 //Modify test data here
 
-const testData = [
+export const pieTestData = [
 	{ group: "g1", value: 1 },
 	{ group: "g2", value: 5, fill: "#A179AA" },
 	{ group: "g3", value: 2, fill: "#C4A3CD" },
@@ -9,4 +9,32 @@ const testData = [
 	{ group: "g6", value: 0, fill: "#E8F3F9" },
 ]
 
-export default testData
+export const lineNumTestData = [
+	{ x: 0, y: 0 },
+	{ x: 10, y: 11 },
+	{ x: 13, y: 19 },
+	{ x: 24, y: 6 },
+	{ x: 31, y: 10 },
+	{ x: 40, y: 33 },
+	{ x: 45, y: 39 },
+	{ x: 55, y: 60 },
+]
+
+export const lineNumNegativeData = [
+	{ x: 0.0, y: -3.7 },
+	{ x: 7.4, y: -11.2 },
+	{ x: 13.8, y: -19.5 },
+	{ x: 24.1, y: -6.9 },
+	{ x: 31.6, y: -28.3 },
+	{ x: 40.2, y: -33.1 },
+]
+
+export const lineDateTestData = [
+	{ x: new Date("2024-01-7"), y: 3 },
+	{ x: new Date("2024-01-14"), y: 19 },
+	{ x: new Date("2024-02-25"), y: 60 },
+	{ x: new Date("2024-01-25"), y: 6 },
+	{ x: new Date("2024-02-01"), y: 10 },
+	{ x: new Date("2024-02-10"), y: 33 },
+	{ x: new Date("2024-02-15"), y: 39 },
+]
